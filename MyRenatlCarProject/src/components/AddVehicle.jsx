22c2b290cc707217f0f17ae99ff6836a4e23b8e0
Gav7830 <48677669+Gav7830@users.noreply.github.com>
2022-10-27 17:24:38 +0300

    Add files via upload

---
+++ MyRenatlCarProject/src/components/AddVehicle.jsx
@@ -0,0 +1,10 @@
+import UpdateFormForVehicle from "./UpdateFormForVehicle";
+
+const AddVehicle = () => {
+
+  return (
+    <div className="addVehicle"><UpdateFormForVehicle mode={"add"} args={null} /></div>
+  );
+};
+
+export default AddVehicle
---
+++ MyRenatlCarProject/src/components/Layout.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+    return (
+        <>
+            <div className="topContainer">
+                <div className="logo">
+                    <img src={'https://raw.githubusercontent.com/Gav7830/ReactJS_MiddleProject/main/logo.png'} alt="I&G Rental Car">
+                    </img>
+                    <h1>I&G Rental Car</h1>
+                </div>
+            </div>
+
+            <div className="topnav">
+                <Link to="/AllVehicles">All Vehicles</Link>
+                <Link to="/AddVehicle">Add Vehicle</Link>
+                <Link to="/AboutUs">About Us</Link>
+            </div>
+
+            <Outlet />
+        </>
+    )
+};
+
+export default Layout;
