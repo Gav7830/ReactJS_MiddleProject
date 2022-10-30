ff26ee675cc9e5dba98b9a574faaf71f7d62c935
Gav7830 <48677669+Gav7830@users.noreply.github.com>
2022-10-30 15:34:08 +0200

    Add files via upload

--- my-rentalcar-app/src/components/Utilities.jsx
+++ my-rentalcar-app/src/components/Utilities.jsx
@@ -5,7 +5,8 @@ const Utilities = {
     getAllCarModels: function(){
         return ["", "1 Series", "GT", "Q7", "i20", "323", "626", "XC40", "S60/V60", "Prius", "Yaris", "C-HR"];
     },
-    ImagesUrl: "https://raw.githubusercontent.com/Gav7830/ReactJS_MiddleProject/main/Pictures/"
+    ImagesUrl: "https://raw.githubusercontent.com/Gav7830/Pictures/main/"
+   
 }
 
 export default Utilities;
